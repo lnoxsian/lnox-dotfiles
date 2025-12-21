05ad5882ed3a8d781f07d0b292754c383dba47e1
lnoxsian <lnox122224@proton.me>
2025-12-21 19:49:53 +0530

    updated the setup to use the new version of treesitter the binary needs to be installed and used
    jsut refer the newer treesitter changes for the binary to be used

--- neovim/.config/nvim/lua/lnoxsian/plug_set/plugins/treesitter.lua
+++ neovim/.config/nvim/lua/lnoxsian/plug_set/plugins/treesitter.lua
@@ -1,53 +1,27 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  event = { "BufReadPre", "BufNewFile" },
-  build = ":TSUpdate",
+  'nvim-treesitter/nvim-treesitter',
+  lazy = false,
+  build = ':TSUpdate',
   config = function()
-    -- import nvim-treesitter plugin
-    local treesitter = require("nvim-treesitter.configs")
-
-    -- configure treesitter
-    treesitter.setup({ -- enable syntax highlighting
-      highlight = {
-        enable = true,
-      },
-      -- enable indentation
-      indent = { enable = true },
-      -- ensure these language parsers are installed
-      ensure_installed = {
-        "json",
-        "javascript",
-        "typescript",
-        "tsx",
-        "yaml",
-        "html",
-        "css",
-        "markdown",
-        "markdown_inline",
-        "bash",
-        "lua",
-        "vim",
-        "dockerfile",
-        "gitignore",
-        "vimdoc",
-        "c",
-        "python",
-        "rust",
-        "zig",
-        "go",
-      },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "<C-space>",
-          node_incremental = "<C-space>",
-          scope_incremental = false,
-          node_decremental = "<bs>",
-        },
-      },
+    local treesitter = require("nvim-treesitter")
+    treesitter.install({
+      'json',
+      'yaml',
+      'markdown',
+      'markdown_inline',
+      'javascript', 
+      'typescript',
+      'tsx',
+      'html',
+      'css',
+      'c',
+      'zig',
+      'go',
+      'rust', 
+      'python',
+      'dockerfile',
+      'vim',
+      'lua'
     })
-
-    -- use bash parser for zsh files
-    vim.treesitter.language.register("bash", "zsh")
-  end,
+  end
 }
