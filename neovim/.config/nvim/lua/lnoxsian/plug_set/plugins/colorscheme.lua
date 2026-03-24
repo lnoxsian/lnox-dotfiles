d61bfb867cfe4e4229a2a74e5e8a46f317877d93
lnoxsian <lnox122224@tutamail.com>
2026-03-24 23:12:57 +0530

    fixed some of the plugins and setup of the dotfiles is completed

--- neovim/.config/nvim/lua/lnoxsian/plug_set/plugins/colorscheme.lua
+++ neovim/.config/nvim/lua/lnoxsian/plug_set/plugins/colorscheme.lua
@@ -1,35 +1,73 @@
+-- lua/plugins/gruvbox.lua
 return {
-
-  "ellisonleao/gruvbox.nvim",
-
+  "motaz-shokry/gruvbox.nvim",
+  name = "gruvbox",
   priority = 1000,
-
   config = function()
-    theme_opt = "gruvbox"
-    local theme = require("gruvbox")
-    theme.setup({
-      terminal_colors = true, -- add neovim terminal colors
-      undercurl = true,
-      underline = true,
-      bold = true,
-      italic = {
-        strings = true,
-        emphasis = true,
-        comments = true,
-        operators = false,
-        folds = true,
+    require("gruvbox").setup({
+      variant = "auto",        -- auto, hard, medium, soft, light
+      dark_variant = "medium", -- hard, medium, soft
+      dim_inactive_windows = true,
+      extend_background_behind_borders = false,
+
+      enable = {
+        terminal = true,
+        migrations = true,        -- Handle deprecated options automatically
+        devicons = true,          -- Theming devicons with gruvbox
+        lualine = true,
       },
-      strikethrough = true,
-      invert_selection = false,
-      invert_signs = false,
-      invert_tabline = false,
-      inverse = true, -- invert background for search, diffs, statuslines and errors
-      contrast = "", -- can be "hard", "soft" or empty string
-      palette_overrides = {},
-      overrides = {},
-      dim_inactive = false,
-      transparent_mode = false,
-    })
-    vim.cmd("colorscheme gruvbox")
-  end,
-}
+
+      styles = {
+        bold = true,
+        italic = true,
+        transparency = false,
+      },
+
+      groups = {
+        -- UI Elements
+        border = "gray",
+        link = "purple_lite",
+        panel = "bg_second",
+
+        -- Diagnostic levels
+        error = "red_lite",
+        hint = "aqua_lite",
+        info = "blue_lite",
+        ok = "green_lite",
+        warn = "yellow_lite",
+        note = "yellow_dark",
+        todo = "aqua_dark",
+
+        -- Git states
+        git_add = "green_dark",
+        git_change = "yellow_dark",
+        git_delete = "red_dark",
+        git_dirty = "orange_dark",
+        git_ignore = "gray",
+        git_merge = "purple_dark",
+        git_rename = "blue_dark",
+        git_stage = "purple_dark",
+        git_text = "yellow_lite",
+        git_untracked = "bg2",
+
+        -- Headings
+        h1 = "red_dark",
+        h2 = "yellow_dark",
+        h3 = "green_dark",
+        h4 = "aqua_dark",
+        h5 = "blue_dark",
+        h6 = "purple_dark",
+      },
+
+      palette = {
+        },
+        highlight_groups = {
+        },
+
+        before_highlight = function(group, highlight, palette)
+        end,
+      })
+
+      vim.cmd("colorscheme gruvbox")
+    end
+  }
